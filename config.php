ceab67c769699c66a27a33617353760bded4c8f6
ZCS <contact.zcsco@gmail.com>
2025-03-05 15:00:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 $db_host = 'localhost';
-$db_user = 'adnan';         // Replace with your MySQL username
+$db_user = 'u324921317_rinsha';         // Replace with your MySQL username
 $db_pass = 'Adnan@66202';             // Replace with your MySQL password
-$db_name = 'village_monitoring';
+$db_name = 'u324921317_villagesys';
 
 // Connect to the database
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
